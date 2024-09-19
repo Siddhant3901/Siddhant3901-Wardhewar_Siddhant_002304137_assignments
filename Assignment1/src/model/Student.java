7c5e95a45b335f5f66a74f1060e0edb23b30f6ab
siddhant wardhewar <wardhewar.s@northeastern.edu>
2024-09-19 16:42:50 -0400

    assighn 1 done

---
+++ Assignment1/src/model/Student.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author siddh
+ */
+public class Student {
+    
+    String fname;
+    String lname;
+    String email;
+    String phone;
+    String DLnum;
+    String DLstate;
+    String ssn;
+    
+    String addL1;
+    String addL2;
+    String city;
+    String state;
+    String zip;
+    String tphone;
+    String faxno;
+    
+    String dob; // date of birth
+    String age; // age
+    String mstatus; //marital status
+    String prof; //profession 
+    String weight;// weight
+    String gender;//gender
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDLnum() {
+        return DLnum;
+    }
+
+    public void setDLnum(String DLnum) {
+        this.DLnum = DLnum;
+    }
+
+    public String getDLstate() {
+        return DLstate;
+    }
+
+    public void setDLstate(String DLstate) {
+        this.DLstate = DLstate;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getAddL1() {
+        return addL1;
+    }
+
+    public void setAddL1(String addL1) {
+        this.addL1 = addL1;
+    }
+
+    public String getAddL2() {
+        return addL2;
+    }
+
+    public void setAddL2(String addL2) {
+        this.addL2 = addL2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTphone() {
+        return tphone;
+    }
+
+    public void setTphone(String tphone) {
+        this.tphone = tphone;
+    }
+
+    public String getFaxno() {
+        return faxno;
+    }
+
+    public void setFaxno(String faxno) {
+        this.faxno = faxno;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getMstatus() {
+        return mstatus;
+    }
+
+    public void setMstatus(String mstatus) {
+        this.mstatus = mstatus;
+    }
+
+    public String getProf() {
+        return prof;
+    }
+
+    public void setProf(String prof) {
+        this.prof = prof;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+}
